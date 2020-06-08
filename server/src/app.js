c854b159d2d9c18dad9cad7d6713727113dae796
Martin Albert <martin.albert.187@gmail.com>
2020-06-08 14:33:41 +0200

    removed semicolons

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,9 @@
 // package routes
-const express = require('express');
-const app = express();
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const { graphqlExpress } = require('apollo-server-express');
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const { graphqlExpress } = require('apollo-server-express')
 const { makeExecutableSchema } = require('graphql-tools')
 const typeDefs = require('./schema')
 const resolvers = require('./resolvers')
@@ -14,29 +14,29 @@ const schema = makeExecutableSchema({
 })
 
 // middlewares
-app.use(morgan('dev'));
+app.use(morgan('dev'))
 app.use(bodyParser.json())
 
 // Access Control Allow Origin
 // Basic Headers
 app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+	res.header('Access-Control-Allow-Origin', '*')
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
 	
 	if(req.method === 'OPTIONS') {
-		res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-		return res.status(200).json({});
+		res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+		return res.status(200).json({})
 	}
-	next();
+	next()
 })
 
 // Server homepage http://127.0.0.1/
 app.get('/', function(req, res) {
-	res.status(200).send('Hello World!');
-});  
+	res.status(200).send('Hello World!')
+})  
 
 // Server GraphQL
-app.use('/graphql', graphqlExpress({ schema: myGraphQLSchema }));
+app.use('/graphql', graphqlExpress({ schema: myGraphQLSchema }))
 
-module.exports = app;
+module.exports = app
 module.exports.schema = schema
