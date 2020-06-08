fb747ce06af89d8093b60035b403a0cae4ee8e63
Martin Albert <martin.albert.187@gmail.com>
2020-06-08 23:52:25 +0200

    Revert "added new mutation functions into scheme"
    This reverts commit 27556f03ebf48540e95302ff1d910201672051cd.

--- server/src/schema.js
+++ server/src/schema.js
@@ -34,10 +34,6 @@ const Mutation = `
     type Mutation {
         createReading(timestamp: String!, reading: Int!): Reading!
         deleteReading(timestamp: String!): Boolean!
-        
-        createPerson(email: String!, firstName: String!, lastName: String!, department: String!, age: Int): Person!
-        deletePerson(email: String!): Boolean!
-        updatePerson(email: String!, firstName: String, lastName: String, department: String, age: Int): Boolean!
     }
 `
 
