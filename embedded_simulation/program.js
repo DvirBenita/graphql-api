362cf218b4bd6a7b0587e5261f55e93bb21e1570
Martin Albert <albert@killkitsch.com>
2020-06-08 12:29:11 +0200

    created http client

---
+++ embedded_simulation/program.js
@@ -0,0 +1,51 @@
+const http = require('http')
+const fs = require('fs')
+
+function getReading() {
+    return JSON.stringify(
+        { 
+            "timestamp": Date.now(),
+            "reading": Math.floor(Math.random() * 350) - 100
+        }
+    )
+}
+
+const server_url = '127.0.0.1'
+const options = {
+    hostname: server_url,
+    port: 3000,
+    path: '/readings',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+}
+
+function post(data) {
+
+    const request = http.request(options, res => {
+        res.setEncoding('utf8')
+    
+        res.pipe(fs.createWriteStream('./response.log'))
+    
+        res.on('end', () => {
+            console.log('no more data in response')
+        })
+    })
+
+    request.on('error', err => {
+        console.error(err.message)
+    })
+
+    request.write(data)
+    request.end()
+
+}
+
+setInterval(() => {
+    post(getReading())
+}, 1000)
+
+
+
+
