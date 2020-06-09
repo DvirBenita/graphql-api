f5ab4c1a633007208813835b0d2069c1ccedb460
Martin Albert <martin.albert.187@gmail.com>
2020-06-09 15:05:54 +0200

    distributed generating functions to other files and created check for positive and negative reading only

--- embedded_simulation/program.js
+++ embedded_simulation/program.js
@@ -1,50 +1,52 @@
 const http = require('http')
 const fs = require('fs')
+const { getReading, getScan } = require('./generate')
 
-function getReading() {
-    return JSON.stringify(
-        { 
-            "timestamp": Date.now(),
-            "reading": Math.floor(Math.random() * 350) - 100
-        }
-    )
-}
-
+const interval = 10000 // 10 seconds
 const serverUrl = '127.0.0.1'
 const options = {
     hostname: serverUrl,
     port: 3000,
-    path: '/readings',
+    path: '/graphql',
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     }
 }
 
-function post(data) {
+const post = (data) => {
 
     const request = http.request(options, res => {
         res.setEncoding('utf8')
     
-        res.pipe(fs.createWriteStream('./response.log'))
+        fs.createReadStream('./response.log').pipe(res.pipe(fs.createWriteStream('./response.log')))
     
         res.on('end', () => {
-            console.log('no more data in response')
+            console.log('request have ended')
         })
     })
 
     request.on('error', err => {
         console.error(err.message)
     })
-
     request.write(data)
     request.end()
 
 }
 
+let pivot = 0
 setInterval(() => {
-    post(getReading())
-}, 1000)
+    pivot += 27
+
+    const reading = getReading(pivot % 200)
+    const scan = getScan(pivot)
+
+    if (reading)
+        post(reading)
+
+    post(scan)
+    
+}, interval)
 
 
 
