bd9e5a654d623aebc7a6f926a62ee341de2e29e7
Martin Albert <martin.albert.187@gmail.com>
2020-06-09 03:02:38 +0200

    Created and connected Redis DAOs to resolvers

--- server/src/models.js
+++ server/src/models.js
@@ -1,35 +1,226 @@
+const redis = require('./redisClient');
+const _ = require('lodash')
 
-let readingsList = {
-    1591643927103: {
-        timestamp: new Date(1591643927103),
-        date: new Date(1591643927103).toUTCString(),
-        reading: 25
-    },
-    1591644336681: {
-        timestamp: new Date(1591644336681),
-        date: new Date(1591644336681).toUTCString(),
-        reading: -100
+const readingsSetKey = 'Readings:Set'
+const peopleSetKey = 'People:Set'
+
+/**
+ * Returns timestamp and UTC String of actual Time
+ */
+const getCurrentTime = () => {
+    return {
+        currentT: Date.now(),
+        currentD: new Date(Date.now()).toUTCString()
     }
 }
 
-let peopleList = {
-    'martin.albert@gmail.com': {
-        email: 'martin.albert@gmail.com',
-        firstName: 'Martin',
-        lastName: 'Albert',
-        department: 'IT',
-        age: 21
-    },
-    'peter.albert@gmail.com': {
-        email: 'peter.albert@gmail.com',
-        firstName: 'Peter',
-        lastName: 'Albert',
-        department: 'Civil Engineering',
-        age: 25
-    },
+/**
+ * Gets the Person object for a given email.
+ */
+const getPerson = async email => {
+
+    if (email === undefined)
+        return {
+            email: 'Not Provided',
+            firstName: 'Not Provided',
+            lastName: 'Not Provided',
+            department: 'Not Provided',
+            age: null
+        }
+
+    const client = redis.getClient()
+    const personKey = `${email}:Person`
+    const person = await client.hgetallAsync(personKey)
+
+    return person
+}
+/**
+ * Gets all Person objects.
+ */
+const getAllPeople = async () => {
+
+    const client = redis.getClient()
+    const personKeys = await client.smembersAsync(peopleSetKey)
+    const people = []
+
+    for (const key of personKeys) {
+        const person = await client.hgetallAsync(key)
+
+        if (person)
+            people.push(person)
+    }
+
+    return people
+}
+/**
+ * Creates new Person object with given attributes.
+ */
+const createPerson = async (email, firstName, lastName, department, age) => {
+
+    const client = redis.getClient()
+    const personKey = `${email}:Person`
+
+    // checking duplicates
+    if (await client.hgetAsync(personKey, 'email') !== null)
+        return {
+            email: 'Already Exists',
+            firstName: 'Already Exists',
+            lastName: 'Already Exists',
+            department: 'Already Exists',
+            age: null
+        }
+
+    const newPerson = {
+        email: email,
+        firstName: firstName,
+        lastName: lastName,
+        department: department,
+        age: age || 0
+    }
+
+    await client.saddAsync(peopleSetKey, personKey)
+    await client.hmsetAsync(personKey, newPerson)
+
+    return newPerson
+}
+/**
+ * Updates the Person object with new attributes.
+ */
+const updatePerson = async (email, firstName, lastName, department, age) => {
+
+    const client = redis.getClient()
+    const personKey = `${email}:Person`
+
+    const newAttrs = {
+        email: email,
+        firstName: firstName,
+        lastName: lastName,
+        department: department,
+        age: age
+    }
+    const oldPerson = await client.hgetallAsync(personKey)
+
+    const updatedPerson = {}
+
+    _.forEach(oldPerson, (attr, key) => {
+        if (newAttrs[key] === undefined || newAttrs[key] === attr)
+            updatedPerson[key] = attr
+        else 
+            updatedPerson[key] = newAttrs[key]
+    })
+
+    if (email !== oldPerson['email']){
+        await client.sremAsync(peopleSetKey, oldPerson['email'])
+        await client.saddAsync(peopleSetKey, personKey)
+    }
+    
+    const result = await client.hmsetAsync(personKey, updatedPerson)
+    
+    return result === 'OK' ? true : false
+}
+/**
+ * Deletes the Person object represented by given email.
+ */
+const deletePerson = async email => {
+    const client = redis.getClient()
+    const personKey = `${email}:Person`
+
+    await client.sremAsync(peopleSetKey, personKey)
+    const result = await client.delAsync(personKey)
+    
+    return result ? true : false    
+}
+/**
+ * Gets the Reading object for a given timestamp.
+ */
+const getReading = async timestamp => {
+
+    const { currentT, currentD } = getCurrentTime()
+    if (timestamp === undefined)
+        return {
+            timestamp: currentT,
+            date: currentD,
+            reading: 0,
+        }
+
+    const client = redis.getClient()
+    
+    timestamp = Number(timestamp)
+    const readingKey = `${timestamp}:Reading`
+
+    const reading = client.hgetallAsync(readingKey)
+    return reading
+
+}
+/**
+ * Gets all Reading objects.
+ */
+const getAllReadings = async () => {
+
+    const client = redis.getClient()
+    const readingKeys = await client.smembersAsync(readingsSetKey)
+    const readings = []
+
+    for (const key of readingKeys) {
+        const reading = await client.hgetallAsync(key)
+
+        if (reading)
+            readings.push(reading)
+    }
+
+    return readings
+}
+/**
+ * Creates new Reading object with given attributes.
+ */
+const createReading = async (timestamp, reading) => {
+    
+    const client = redis.getClient()
+    const { currentT, currentD } = getCurrentTime()
+    
+    timestamp = Number(timestamp)
+    const readingKey = `${timestamp}:Reading`
+
+    // checking duplicates
+    if (await client.hgetAsync(readingKey, 'reading') !== null)
+        return {
+            timestamp: currentT,
+            date: currentD,
+            reading: 0,
+        }
+
+    const newReading = {
+        timestamp: new Date(timestamp),
+        date: new Date(timestamp).toUTCString(),
+        reading: reading
+    }
+    
+    await client.saddAsync(readingsSetKey, readingKey)
+    await client.hmsetAsync(readingKey, newReading)
+    return newReading
+}
+/**
+ * Deletes the Reading object represented by given email.
+ */
+const deleteReading = async timestamp => {
+    const client = redis.getClient()
+    const readingKey = `${timestamp}:Reading`
+
+    await client.sremAsync(readingsSetKey, readingKey)
+    const result = await client.delAsync(readingKey)
+    
+    return result ? true : false    
 }
 
 module.exports = {
-    readingsList: readingsList,
-    peopleList: peopleList
+    getPerson,
+    getAllPeople,
+    createPerson,
+    updatePerson,
+    deletePerson,
+
+    getReading,
+    getAllReadings,
+    createReading,
+    deleteReading,
 }
--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,120 +1,27 @@
 const GraphQLTimestamp = require('./GraphQLTimestamp')
-const CURRENT_TIMESTAMP = Date.now()
-const CURRENT_DATE = new Date(CURRENT_TIMESTAMP).toUTCString()
 
 const resolvers = {
     Timestamp: GraphQLTimestamp,
     Query: {
-        reading: (parent, { timestamp }, { models }) => {
+        reading: (parent, { timestamp }, { models }) => models.getReading(timestamp),
 
-            if (timestamp !== undefined)
-                return models.readingsList[Number(timestamp)]
-            else 
-                return {
-                    timestamp: CURRENT_TIMESTAMP,
-                    date: CURRENT_DATE,
-                    reading: 0,
-                }
-        },
+        readings: (parent, args, { models }) => models.getAllReadings(),
 
-        readings: (parent, args, { models }) => Object.values(models.readingsList),
+        person: (parent, { email }, { models }) => models.getPerson(email),
 
-        person: (parent, { email }, { models }) => {
-            if (email !== undefined)
-                return models.peopleList[email]
-            else 
-                return {
-                    email: 'Not Provided',
-                    firstName: 'Not Provided',
-                    lastName: 'Not Provided',
-                    department: 'Not Provided',
-                }
-        },
-
-        people: (parent, args, { models }) => Object.values(models.peopleList)
+        people: (parent, args, { models }) => models.getAllPeople()
     },
 
     Mutation: {
-        createReading: (parent, { timestamp, reading }, { models }) => {
-            timestamp = Number(timestamp)
+        createReading: (parent, { timestamp, reading }, { models }) => models.createReading(timestamp, reading),
+        
+        deleteReading: (parent, { timestamp }, { models }) => models.deleteReading(timestamp),
 
-            // checking duplicates
-            if (models.readingsList[timestamp] !== undefined)
-                return {
-                    timestamp: CURRENT_TIMESTAMP,
-                    date: CURRENT_DATE,
-                    reading: 0,
-                }
+        createPerson: (parent, { email, firstName, lastName, department, age }, { models }) => models.createPerson(email, firstName, lastName, department, age),
+        
+        deletePerson: (parent, { email }, { models }) => models.deletePerson(email),
 
-            const newReading = {
-                timestamp: new Date(timestamp),
-                date: new Date(timestamp).toUTCString(),
-                reading: reading
-            }
-            
-            models.readingsList[timestamp] = newReading
-            return newReading
-        },
-        deleteReading: (parent, { timestamp }, { models }) => {
-            timestamp = Number(timestamp)
-
-            const { [timestamp]: reading, ...otherReadings } = models.readingsList
-            
-            if (!reading)
-                return false
-            
-            models.readingsList = otherReadings
-            return true
-        },
-
-        createPerson: (parent, { email, firstName, lastName, department, age }, { models }) => {
-            
-            // checking duplicates
-            if (models.peopleList[email] !== undefined) 
-                return {
-                    email: 'Not Provided',
-                    firstName: 'Not Provided',
-                    lastName: 'Not Provided',
-                    department: 'Not Provided',
-                }
-            
-            const newPerson = {
-                email: email,
-                firstName: firstName,
-                lastName: lastName,
-                department: department,
-                age: age
-            }
-
-            models.peopleList[email] = newPerson
-            return newPerson
-        },
-        deletePerson: (parent, { email }, { models }) => {
-
-            const { [email]: person, ...otherPeople } = models.peopleList
-            
-            if (!person)
-                return false
-            
-            models.peopleList = otherPeople
-            return true
-        },
-        updatePerson: (parent, { email, firstName, lastName, department, age }, { models }) => {
-            
-            if (models.peopleList[email] === undefined) 
-                return false
-
-            const updatedPerson = {
-                email: email,
-                firstName: firstName,
-                lastName: lastName,
-                department: department,
-                age: age
-            }
-
-            models.peopleList[email] = updatedPerson
-            return true
-        }
+        updatePerson: (parent, { email, firstName, lastName, department, age }, { models }) => models.updatePerson(email, firstName, lastName, department, age),
     }
 }
 
